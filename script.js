cc7b1dd5ba145afaf80e6db0985cbb1fed6f665e
MIM-0014 <fatemaaktermim1031@gmail.com>
2026-03-04 11:33:33 +0600

    added job data

---
+++ script.js
@@ -0,0 +1,11 @@
+// ================= JOB DATA =================
+const jobs = [
+  { id: 1, companyName: "Google", position: "Frontend Developer", location: "Remote", type: "Full-Time", salary: "$120k/year", description: "Develop scalable user interfaces for global products.", status: "all" },
+  { id: 2, companyName: "Microsoft", position: "Backend Engineer", location: "USA", type: "Full-Time", salary: "$130k/year", description: "Build secure and scalable cloud APIs.", status: "all" },
+  { id: 3, companyName: "Tesla", position: "React Developer", location: "California", type: "Contract", salary: "$100k/year", description: "Work on EV dashboard interface systems.", status: "all" },
+  { id: 4, companyName: "Amazon", position: "Node.js Developer", location: "Canada", type: "Full-Time", salary: "$115k/year", description: "Develop high-performance backend services.", status: "all" },
+  { id: 5, companyName: "Netflix", position: "UI Engineer", location: "Remote", type: "Full-Time", salary: "$125k/year", description: "Improve streaming platform UI experience.", status: "all" },
+  { id: 6, companyName: "Meta", position: "Software Engineer", location: "UK", type: "Full-Time", salary: "$140k/year", description: "Build scalable social media systems.", status: "all" },
+  { id: 7, companyName: "Spotify", position: "Frontend Engineer", location: "Sweden", type: "Hybrid", salary: "$110k/year", description: "Design music streaming web interface.", status: "all" },
+  { id: 8, companyName: "Adobe", position: "Full Stack Developer", location: "Germany", type: "Full-Time", salary: "$135k/year", description: "Develop creative cloud web applications.", status: "all" }
+];
